5cd73cf5b3b1ee798f5f5eb679069f7ed6290879
Muhammet Albayrak <mhasanalbyrk@gmail.com>
2022-01-09 18:30:48 +0300

    pywin package will be used instedad win32

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from keras.models import load_model
 from tkinter import *
 import tkinter as tk
-import win32gui
+from pywin32 import win32gui
 from PIL import ImageGrab, Image
 import numpy as np
 model = load_model('mnist.h5')
