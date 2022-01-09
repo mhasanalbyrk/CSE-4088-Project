654e7d2ea4c856712e7b20658f193c66bd39661b
umut- <umutbayram@mail.com>
2022-01-09 13:27:16 +0300

    asdasdasds

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import requests
 import torchvision
-import matplotlib.pyplot as plt
-from time import time
+import torch.nn.functional as F
+from PIL import Image
+from io import BytesIO
 from torchvision import datasets, transforms
-from torch import nn, optim
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,)),
                                 ])
-
+T = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor()
+    ])
 
 def view_classify(img, ps):
     ''' Function for viewing an image and it's predicted classes.
@@ -27,103 +31,139 @@ def view_classify(img, ps):
     ax2.set_xlim(0, 1.1)
     plt.tight_layout()
 
+def inference(path, model, device):
+    r = requests.get(path)
+    with BytesIO(r.content) as f:
+        img = Image.open(f).convert(mode="L")
+        img = img.resize((28, 28))
+        x = (255 - np.expand_dims(np.array(img), -1))/255.
+        img = T(x).view(1, 784)
+    with torch.no_grad():
+        pred = model(torch.unsqueeze(img, axis=0).float().to(device))
+        return F.softmax(pred, dim=-1).cpu().numpy()
 
-trainset = datasets.MNIST('data', download=True, train=True, transform=transform)
-valset = datasets.MNIST('data', download=True, train=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+if __name__ == "__main__":
 
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+    trainset = datasets.MNIST('data', download=True, train=True, transform=transform)
+    valset = datasets.MNIST('data', download=True, train=False, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+    valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 
-print(images.shape)
-print(labels.shape)
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
 
-plt.imshow(images[0].numpy().squeeze(), cmap='gray_r');
+    img = Image.open("2.jpg")
+    convert_tensor =transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                                ])
+    sayi = convert_tensor(img)
 
-figure = plt.figure()
-num_of_images = 60
-for index in range(1, num_of_images + 1):
-    plt.subplot(6, 10, index)
-    plt.axis('off')
-    plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
-
-    input_size = 784
-    hidden_sizes = [128, 64]
-    output_size = 10
-
-    model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                          nn.ReLU(),
-                          nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-                          nn.ReLU(),
-                          nn.Linear(hidden_sizes[1], output_size),
-                          nn.LogSoftmax(dim=1))
-    print(model)
-
-    criterion = nn.NLLLoss()
-    images, labels = next(iter(trainloader))
-    images = images.view(images.shape[0], -1)
-
-    logps = model(images)  # log probabilities
-    loss = criterion(logps, labels)  # calculate the NLL loss
-
-    print('Before backward pass: \n', model[0].weight.grad)
-    loss.backward()
-    print('After backward pass: \n', model[0].weight.grad)
-
-optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
-time0 = time()
-epochs = 15
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
-
-        # Training pass
-        optimizer.zero_grad()
-
-        output = model(images)
-        loss = criterion(output, labels)
-
-        # This is where the model learns by backpropagating
-        loss.backward()
-
-        # And optimizes its weights here
-        optimizer.step()
-
-        running_loss += loss.item()
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
     else:
-        print("Epoch {} - Training loss: {}".format(e, running_loss / len(trainloader)))
-print("\nTraining Time (in minutes) =", (time() - time0) / 60)
+        device = torch.device("cpu")
 
-images, labels = next(iter(valloader))
+    path = "https://c8.alamy.com/comp/HXBRW0/2-red-handwritten-digits-over-white-background-HXBRW0.jpg"
 
-img = images[0].view(1, 784)
-with torch.no_grad():
-    logps = model(img)
 
-ps = torch.exp(logps)
-probab = list(ps.numpy()[0])
-print("Predicted Digit =", probab.index(max(probab)))
-view_classify(img.view(1, 28, 28), ps)
 
-correct_count, all_count = 0, 0
-for images, labels in valloader:
-    for i in range(len(labels)):
-        img = images[i].view(1, 784)
-        with torch.no_grad():
-            logps = model(img)
+    print(images.shape)
+    print(labels.shape)
 
-        ps = torch.exp(logps)
-        probab = list(ps.numpy()[0])
-        pred_label = probab.index(max(probab))
-        true_label = labels.numpy()[i]
-        if (true_label == pred_label):
-            correct_count += 1
-        all_count += 1
+    plt.imshow(images[0].numpy().squeeze(), cmap='gray_r');
 
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count / all_count))
+    figure = plt.figure()
+    num_of_images = 60
+    # for index in range(1, num_of_images + 1):
+    #     plt.subplot(6, 10, index)
+    #     plt.axis('off')
+    #     # plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
+    #
+    #     input_size = 784
+    #     hidden_sizes = [128, 64]
+    #     output_size = 10
+    #
+    #     model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+    #                           nn.ReLU(),
+    #                           nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+    #                           nn.ReLU(),
+    #                           nn.Linear(hidden_sizes[1], output_size),
+    #                           nn.LogSoftmax(dim=1))
+    #     print(model)
+    #
+    #     criterion = nn.NLLLoss()
+    #     images, labels = next(iter(trainloader))
+    #     images = images.view(images.shape[0], -1)
+    #
+    #     logps = model(images)  # log probabilities
+    #     loss = criterion(logps, labels)  # calculate the NLL loss
+    #
+    #     print('Before backward pass: \n', model[0].weight.grad)
+    #     loss.backward()
+    #     print('After backward pass: \n', model[0].weight.grad)
+    #
+    # optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+    # time0 = time()
+    # epochs = 15
+    # for e in range(epochs):
+    #     running_loss = 0
+    #     for images, labels in trainloader:
+    #         # Flatten MNIST images into a 784 long vector
+    #         images = images.view(images.shape[0], -1)
+    #
+    #         # Training pass
+    #         optimizer.zero_grad()
+    #
+    #         output = model(images)
+    #         loss = criterion(output, labels)
+    #
+    #         # This is where the model learns by backpropagating
+    #         loss.backward()
+    #
+    #         # And optimizes its weights here
+    #         optimizer.step()
+    #
+    #         running_loss += loss.item()
+    #     else:
+    #         print("Epoch {} - Training loss: {}".format(e, running_loss / len(trainloader)))
+    # print("\nTraining Time (in minutes) =", (time() - time0) / 60)
 
-torch.save(model, './my_mnist_model.pt')
+
+    model = torch.load("my_mnist_model.pt")
+    model.eval()
+
+    pred = inference(path, model, device)
+    pred_idx = np.argmax(pred)
+    print(f"Predicted: {pred_idx}, Prob: {pred[0][pred_idx] * 100} %")
+
+    images, labels = next(iter(valloader))
+
+    img = images[0].view(1, 784)
+
+    with torch.no_grad():
+        logps = model(img)
+
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    print("Predicted Digit =", probab.index(max(probab)))
+    view_classify(img.view(1, 28, 28), ps)
+
+    correct_count, all_count = 0, 0
+    for images, labels in valloader:
+        for i in range(len(labels)):
+            img = images[i].view(1, 784)
+            with torch.no_grad():
+                logps = model(img)
+
+            ps = torch.exp(logps)
+            probab = list(ps.numpy()[0])
+            pred_label = probab.index(max(probab))
+            true_label = labels.numpy()[i]
+            if (true_label == pred_label):
+                correct_count += 1
+            all_count += 1
+
+    print("Number Of Images Tested =", all_count)
+    print("\nModel Accuracy =", (correct_count / all_count))
+
+    torch.save(model, './my_mnist_model.pt')
